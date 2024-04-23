447ff604957c70bfef897456a27b2da7b981ebe7
CNERD <cnerd694@gmail.com>
2024-04-23 14:43:14 +0100

    exported errors

--- src/bcs.nim
+++ src/bcs.nim
@@ -3,7 +3,7 @@
 #
 #      See the file "LICENSE", included in this
 #    distribution, for details about the copyright.
-import bcs / [constants, serializer, deserializer, largeints, hex]
+import bcs / [constants, serializer, deserializer, largeints, hex, errors]
 
-export constants, serializer, deserializer, largeints, hex
+export constants, serializer, deserializer, largeints, hex, errors
 

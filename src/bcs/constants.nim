312cb539d57e066e8ab6487844ec507daf2485a1
CNERD <cnerd694@gmail.com>
2024-08-17 01:46:07 +0100

    removed  type from constants

--- src/bcs/constants.nim
+++ src/bcs/constants.nim
@@ -3,11 +3,6 @@
 #
 #      See the file "LICENSE", included in this
 #    distribution, for details about the copyright.
-from std / tables import CountTable, CountTableRef, OrderedTable,
-        OrderedTableRef, Table, TableRef
-from std / options import Option
-from largeints import int128, uint128, int256, uint256
-
 type
 
     InvalidSequenceLength* = object of RangeDefect
@@ -15,10 +10,6 @@ type
     InvalidBcsStructure* = object of CatchableError
 
     ContainerDepthError* = object of RangeDefect
-
-    Serializables* = CountTable | CountTableRef | OrderedTable |
-            OrderedTableRef | Table | TableRef | int128 | uint128 | int256 |
-            uint256 | bool | array | seq | SomeInteger | string | enum | Option
 
 const
     MAX_CONTAINER_DEPTH* = 500 ## container depth will be the nim compilers recursion depth

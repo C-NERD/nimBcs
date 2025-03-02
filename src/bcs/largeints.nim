15a593508f7e75e4a8e170ac139ecb4a47382a73
CNERD <cnerd694@gmail.com>
2025-03-02 13:27:41 +0100

    implemented nimalyzer rules for largeints module

--- src/bcs/largeints.nim
+++ src/bcs/largeints.nim
@@ -12,58 +12,29 @@ import errors
 ##   Emergency int128, uint128, int256 and uint256 implementation for the bcs library.
 ##   When these ints arrive to nim this module will be modified to accomodate them or be scraped completly.
 
-when not defined(js):
-
-    import pkg / integers
-
-    type
-
-        int128* = object ## Emergency int128 type for nim
-
-            value: Integer ## caps value at 170141183460469231731687303715884105727
-                         ## and allows for -ve value
-        uint128* = object ## Emergency uint128 type for nim
-
-            value: Integer ## caps value at 340282366920938463463374607431768211455
-                          ## and does not allow for -ve value
-
-        int256* = object ## Emergency int256 type for nim
-
-            value: Integer
-
-        uint256* = object ## Emergency uint256 type for nim
-
-            value: Integer
-
-    template `int`(data: Integer): untyped = parseInt($data)
-
-    proc `<=`*(x, y: int128 | uint128 | int256 | uint256): bool =
-
-        x.value <= y.value
-
-    proc `<`*(x, y: int128 | uint128 | int256 | uint256): bool =
-
-        x.value < y.value
-
-else:
+when defined(js):
 
     import std / jsbigints
 
     type
 
-        int128* = object ## Emergency int128 type for nim's js backend
+        int128* = object
+            ## Emergency int128 type for nim's js backend
 
             value: JsBigInt
 
-        uint128* = object ## Emergency uint128 type for nim's js backend
+        uint128* = object
+            ## Emergency uint128 type for nim's js backend
 
             value: JsBigInt
 
-        int256* = object ## Emergency int256 type for nim's js backend
+        int256* = object
+            ## Emergency int256 type for nim's js backend
 
             value: JsBigInt
 
-        uint256* = object ## Emergency uint256 type for nim's js backend
+        uint256* = object
+            ## Emergency uint256 type for nim's js backend
 
             value: JsBigInt
 
@@ -88,50 +59,87 @@ else:
 
         x.value < y.value
 
+else:
+
+    import pkg / integers
+
+    type
+
+        int128* = object
+            ## Emergency int128 type for nim
+
+            value: Integer ## caps value at 170141183460469231731687303715884105727
+ ## and allows for -ve value
+        uint128* = object
+            ## Emergency uint128 type for nim
+
+            value: Integer ## caps value at 340282366920938463463374607431768211455
+ ## and does not allow for -ve value
+
+        int256* = object
+            ## Emergency int256 type for nim
+
+            value: Integer
+
+        uint256* = object
+            ## Emergency uint256 type for nim
+
+            value: Integer
+
+    template `int`(data: Integer): untyped = parseInt($data)
+
+    proc `<=`*(x, y: int128 | uint128 | int256 | uint256): bool =
+
+        x.value <= y.value
+
+    proc `<`*(x, y: int128 | uint128 | int256 | uint256): bool =
+
+        x.value < y.value
+
 const HexChars = "0123456789ABCDEF"
 converter `$`*(data: int128): string =
     ## converts int128 type to string
 
-    when not defined(js):
+    when defined(js):
 
-        `$`(data.value)
+        $(toCstring(data.value))
 
     else:
 
-        $(toCstring(data.value))
+        `$`(data.value)
 
 converter `$`*(data: uint128): string =
     ## converts uint128 type to string
 
-    when not defined(js):
+    when defined(js):
 
-        `$`(data.value)
+        $(toCstring(data.value))
 
     else:
 
-        $(toCstring(data.value))
+        `$`(data.value)
 
 converter `$`*(data: int256): string =
     ## converts int256 type to string
 
-    when not defined(js):
+    when defined(js):
 
-        `$`(data.value)
+        $(toCstring(data.value))
 
     else:
 
-        $(toCstring(data.value))
+        `$`(data.value)
 
 converter `$`*(data: uint256): string =
     ## converts uint256 type to string
 
-    when not defined(js):
+    when defined(js):
 
-        `$`(data.value)
+        $(toCstring(data.value))
 
     else:
 
-        $(toCstring(data.value))
+        `$`(data.value)
 
 template pad128Hex(data: var string): untyped =
     ## pads hex representations of int128 and uint128 types with 0's if not up to 32 chars in length
@@ -210,76 +218,76 @@ proc high*[T: int128 | uint128 | int256 | uint256](_: typedesc[T]): T =
 
     when T is int128:
 
-        when not defined(js):
-
-            result.value = 170141183460469231731687303715884105727'gmp
-
-        else:
+        when defined(js):
 
             result.value = 170141183460469231731687303715884105727'big
 
+        else:
+
+            result.value = 170141183460469231731687303715884105727'gmp
+
     elif T is uint128:
 
-        when not defined(js):
-
-            result.value = 340282366920938463463374607431768211455'gmp
-
-        else:
+        when defined(js):
 
             result.value = 340282366920938463463374607431768211455'big
 
+        else:
+
+            result.value = 340282366920938463463374607431768211455'gmp
+
     elif T is int256:
 
-        when not defined(js):
-
-            result.value = 57896044618658097711785492504343953926634992332820282019728792003956564819967'gmp
-
-        else:
+        when defined(js):
 
             result.value = 57896044618658097711785492504343953926634992332820282019728792003956564819967'big
 
+        else:
+
+            result.value = 57896044618658097711785492504343953926634992332820282019728792003956564819967'gmp
+
     elif T is uint256:
 
-        when not defined(js):
+        when defined(js):
 
-            result.value = 115792089237316195423570985008687907853269984665640564039457584007913129639935'gmp
+            result.value = 115792089237316195423570985008687907853269984665640564039457584007913129639935'big
 
         else:
 
-            result.value = 115792089237316195423570985008687907853269984665640564039457584007913129639935'big
+            result.value = 115792089237316195423570985008687907853269984665640564039457584007913129639935'gmp
 
 proc low*[T: int128 | uint128 | int256 | uint256](_: typedesc[T]): T =
     ## gets the lowest possible value for int type
 
     when T is int128:
 
-        when not defined(js):
-
-            result.value = -170141183460469231731687303715884105727'gmp
-
-        else:
+        when defined(js):
 
             result.value = -170141183460469231731687303715884105727'big
 
+        else:
+
+            result.value = -170141183460469231731687303715884105727'gmp
+
     elif T is uint128 or T is uint256:
 
-        when not defined(js):
-
-            result.value = 0'gmp
-
-        else:
+        when defined(js):
 
             result.value = 0'big
 
+        else:
+
+            result.value = 0'gmp
+
     elif T is int256:
 
-        when not defined(js):
+        when defined(js):
 
-            result.value = -57896044618658097711785492504343953926634992332820282019728792003956564819967'gmp
+            result.value = -57896044618658097711785492504343953926634992332820282019728792003956564819967'big
 
         else:
 
-            result.value = -57896044618658097711785492504343953926634992332820282019728792003956564819967'big
+            result.value = -57896044618658097711785492504343953926634992332820282019728792003956564819967'gmp
 
 proc sizeof*[T: int128 | uint128 | int256 | uint256](_: typedesc[T]): T =
     ## gets bytes size of int type
@@ -301,10 +309,11 @@ proc sizeof*[T: int128 | uint128 | int256 | uint256](_: typedesc[T]): T =
         32'u256
 
 proc newInt128*(data: string): int128 =
+    ## convert int string representation to int128
 
-    when not defined(js):
+    when defined(js):
 
-        result.value = newInteger(data)
+        result.value = big(cstring(parseStrInt(data)))
         if result.value > high(int128).value or result.value < low(int128).value:
 
             raise newException(RangeDefect,
@@ -312,17 +321,18 @@ proc newInt128*(data: string): int128 =
 
     else:
 
-        result.value = big(cstring(parseStrInt(data)))
+        result.value = newInteger(data)
         if result.value > high(int128).value or result.value < low(int128).value:
 
             raise newException(RangeDefect,
                     fmt"{result} is not within the range {low(int128)} and {high(int128)}")
 
 proc newUInt128*(data: string): uint128 =
+    ## convert int string representation to uint128
 
-    when not defined(js):
+    when defined(js):
 
-        result.value = newInteger(data)
+        result.value = big(cstring(parseStrInt(data)))
         if result.value > high(uint128).value or result.value < low(uint128).value:
 
             raise newException(RangeDefect,
@@ -330,17 +340,18 @@ proc newUInt128*(data: string): uint128 =
 
     else:
 
-        result.value = big(cstring(parseStrInt(data)))
+        result.value = newInteger(data)
         if result.value > high(uint128).value or result.value < low(uint128).value:
 
             raise newException(RangeDefect,
                     fmt"{result} is not within the range {low(uint128)} and {high(uint128)}")
 
 proc newInt256*(data: string): int256 =
+    ## convert int string representation to int256
 
-    when not defined(js):
+    when defined(js):
 
-        result.value = newInteger(data)
+        result.value = big(cstring(parseStrInt(data)))
         if result.value > high(int256).value or result.value < low(int256).value:
 
             raise newException(RangeDefect,
@@ -348,23 +359,16 @@ proc newInt256*(data: string): int256 =
 
     else:
 
-        result.value = big(cstring(parseStrInt(data)))
+        result.value = newInteger(data)
         if result.value > high(int256).value or result.value < low(int256).value:
 
             raise newException(RangeDefect,
                     fmt"{result} is not within the range {low(int256)} and {high(int256)}")
 
 proc newUInt256*(data: string): uint256 =
+    ## convert int string representation to uint256
 
-    when not defined(js):
-
-        result.value = newInteger(data)
-        if result.value > high(uint256).value or result.value < low(uint256).value:
-
-            raise newException(RangeDefect,
-                    fmt"{result} is not within the range {low(uint256)} and {high(uint256)}")
-
-    else:
+    when defined(js):
 
         result.value = big(cstring(parseStrInt(data)))
         if result.value > high(uint256).value or result.value < low(uint256).value:
@@ -372,13 +376,33 @@ proc newUInt256*(data: string): uint256 =
             raise newException(RangeDefect,
                     fmt"{result} is not within the range {low(uint256)} and {high(uint256)}")
 
-template `'i128`*(data: string): untyped = newInt128(data)
+    else:
 
-template `'u128`*(data: string): untyped = newUInt128(data)
+        result.value = newInteger(data)
+        if result.value > high(uint256).value or result.value < low(uint256).value:
 
-template `'i256`*(data: string): untyped = newInt256(data)
+            raise newException(RangeDefect,
+                    fmt"{result} is not within the range {low(uint256)} and {high(uint256)}")
 
-template `'u256`*(data: string): untyped = newUInt256(data)
+template `'i128`*(data: string): untyped =
+    ## template to convert int string representation to int128
+
+    newInt128(data)
+
+template `'u128`*(data: string): untyped =
+    ## template to convert int string representation to uint128
+
+    newUInt128(data)
+
+template `'i256`*(data: string): untyped =
+    ## template to convert int string representation to int256
+
+    newInt256(data)
+
+template `'u256`*(data: string): untyped =
+    ## template to convert int string representation to uint256
+
+    newUInt256(data)
 
 proc toHex*(data: int128): string =
     ## converts int128 to hex string representation
@@ -394,7 +418,7 @@ proc toHex*(data: int128): string =
         let shiftValue: Integer = (1'i128).value shl (128'i128).value
         data = shiftValue + data
 
-    var resultHex2: string
+    var resultHex2 = ""
     while data > (0'i128).value:
 
         let
@@ -416,7 +440,7 @@ proc toHex*(data: uint128): string =
         var resultHex1 = "0"
         return pad128Hex(resultHex1)
 
-    var resultHex2: string
+    var resultHex2 = ""
     while data > (0'u128).value:
 
         let
@@ -443,7 +467,7 @@ proc toHex*(data: int256): string =
         let shiftValue = (1'i256).value shl (256'i256).value
         data = shiftValue + data
 
-    var resultHex2: string
+    var resultHex2 = ""
     while data > (0'i256).value:
 
         let
@@ -465,7 +489,7 @@ proc toHex*(data: uint256): string =
         var resultHex1 = "0"
         return pad256Hex(resultHex1)
 
-    var resultHex2: string
+    var resultHex2 = ""
     while data > (0'u256).value:
 
         let
